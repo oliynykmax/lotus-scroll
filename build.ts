d92dde774e02cabddb577e3d37828c945d307f5f
oliynykmax <olejnikmax@gmail.com>
2026-03-21 22:03:41 +0200

    add giraffe favicon

--- build.ts
+++ build.ts
@@ -9,6 +9,7 @@ const html = `<!DOCTYPE html>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>vita-22</title>
+<link rel="icon" href="favicon.svg" type="image/svg+xml">
 <style>
   * { margin: 0; padding: 0; box-sizing: border-box; }
 
